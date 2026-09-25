a880f3d3209bf69b7287c1c68ec1d5ea574e4f68
Vivian Azevedo <vivianazevedo2007@gmail.com>
2026-09-25 11:07:33 -0300

    feat: erros do condominio

---
+++ scr/infra/ErroCondominio.cpp
@@ -0,0 +1,15 @@
+#include "infra/ErroCondominio.h"
+
+using namespace std; 
+
+ErroCondominio:: ErroCondominio (string mensagem) : mensagem_(move(mensagem)){}; 
+
+const char*  ErroCondominio :: what() const noexcept {
+    return mensagem_.c_str();
+}
+
+ErroValidacao :: ErroValidacao(const string& mensagem) : ErroCondominio(mensagem) {}
+
+ErroRegraNegocio :: ErroRegraNegocio (const string& mensagem) : ErroCondominio(mensagem) {}
+
+ErroBanco :: ErroBanco (const string& mensagem) : ErroCondominio(mensagem) {}
